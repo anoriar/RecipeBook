38a77d35aa838eba6a085cc7f420669b253e500f
loginarea <passarea>
2022-04-25 19:26:16 +0300

    view model image

--- app/src/main/java/com/example/recipebook/presentation/binding/RecipeDetailBindingAdapters.kt
+++ app/src/main/java/com/example/recipebook/presentation/binding/RecipeDetailBindingAdapters.kt
@@ -1,10 +1,26 @@
 package com.example.recipebook.presentation.binding
 
+import android.net.Uri
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.documentfile.provider.DocumentFile
+import com.example.recipebook.R
 
 
 @BindingAdapter("numberAsText")
 fun bindNumberAsText(tv: TextView, number: Int){
     tv.text = number.toString()
+}
+
+@BindingAdapter("imageToImageView")
+fun bindImageToImageView(iv: ImageView, imageUri: String){
+    val uri: Uri = imageUri.toUri()
+
+    if (DocumentFile.fromSingleUri(iv.context, uri)?.exists() == true) {
+        iv.setImageURI(uri)
+    }else{
+        iv.setImageResource(R.drawable.image_blank)
+    }
 }
